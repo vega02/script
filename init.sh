6b39616bb92fe6763e38f0c62a352b1c9f3c0aef
vega02 <weichia.chen@gmail.com>
2020-11-20 14:08:43 +0800

    Update init.sh
    test script

--- init.sh
+++ init.sh
@@ -1,1 +1,16 @@
-ls
+echo "export PS1='\[\033[1;31m\]\u@\h:\w\$ \[\033[0m\]'" >> ~/.bashrc;
+echo "alias ls='ls \$LS_OPTIONS -A'" >> ~/.bashrc;
+echo "alias ll='ls \$LS_OPTIONS -lA'" >> ~/.bashrc;
+echo "alias l='ls \$LS_OPTIONS -lA'" >> ~/.bashrc;
+echo "alias h='cd ~;ll'" >> ~/.bashrc;
+echo "alias u='cd ..'" >> ~/.bashrc;
+
+echo "alias aptg='apt-get install'" >> ~/.bashrc;
+echo "alias aptr='apt-get remove'" >> ~/.bashrc;
+echo "alias aptu='apt-get update'" >> ~/.bashrc;
+echo "alias aptc='apt-get clean'" >> ~/.bashrc;
+echo "alias apts='apt-cache search'" >> ~/.bashrc;
+echo "alias n='netstat -ntl'" >> ~/.bashrc;
+echo "alias df='df -BMB'" >> ~/.bashrc;
+echo "TZ='Asia/Taipei'; export TZ" >> ~/.bashrc;
+echo "cd ~"
