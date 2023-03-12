c720548151e85f463c38ded6d6f86908903b9866
Vega <weichia.chen@gmail.com>
2023-03-12 15:32:10 +0800

    add install mariadb-server

--- mail.mysql.sh
+++ mail.mysql.sh
@@ -56,6 +56,7 @@ apt-get update \
   && apt install -y tzdata \
   && dpkg-reconfigure --frontend noninteractive tzdata \
   && apt install -y certbot python3-certbot-apache \
+  && apt install -y mariadb-server \
   && apt install -y mc \
   && groupadd -g 5000 vmail \
   && useradd -u 5000 -g vmail -s /usr/bin/nologin -d /home/vmail -m vmail \
